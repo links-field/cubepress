04c76b146846be537b62dce61386f735282c4738
meganmeiyang <meganmeiyang@users.noreply.github.com>
2022-12-08 13:07:21 +0000

    deploy: c95ff1474593bdb503b0fedd81d79b0e54990c4c

--- assets/account_delete.md.367f3058.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o,a}from"./app.b80f3151.js";const f=JSON.parse('{"title":"Delete Account","description":"","frontmatter":{},"headers":[],"relativePath":"account/delete.md","lastUpdated":1670335111000}'),n={name:"account/delete.md"},s=a("",8),c=[s];function i(r,l,d,h,p,u){return o(),t("div",null,c)}var g=e(n,[["render",i]]);export{f as __pageData,g as default};
---
+++ assets/account_edit.md.f81564cf.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a,a as i}from"./app.d6b0ffe3.js";const m=JSON.parse('{"title":"Edit","description":"","frontmatter":{},"headers":[],"relativePath":"account/edit.md","lastUpdated":1670494556000}'),s={name:"account/edit.md"},o=i("",5),d=[o];function c(n,r,l,p,_,u){return a(),t("div",null,d)}var f=e(s,[["render",c]]);export{m as __pageData,f as default};
