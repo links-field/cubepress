6d2078f2e41fb6c7a8c290a2765d2124383f1ccc
meganmeiyang <meganmeiyang@users.noreply.github.com>
2022-12-20 06:04:10 +0000

    deploy: add38a6794c806993a9fa0182af942f58ea90513

---
+++ assets/account_delete.md.327ccffd.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o,a}from"./app.7410f5df.js";const f=JSON.parse('{"title":"Delete Account","description":"","frontmatter":{},"headers":[],"relativePath":"account/delete.md","lastUpdated":1671516211000}'),n={name:"account/delete.md"},s=a("",8),c=[s];function i(r,l,d,h,p,u){return o(),t("div",null,c)}var g=e(n,[["render",i]]);export{f as __pageData,g as default};
---
+++ assets/account_disable.md.46d18b8a.lean.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o as a,a as o}from"./app.7410f5df.js";const b=JSON.parse('{"title":"Disable account","description":"","frontmatter":{},"headers":[],"relativePath":"account/disable.md","lastUpdated":1671516211000}'),n={name:"account/disable.md"},s=o("",6),i=[s];function c(l,r,d,p,u,_){return a(),e("div",null,i)}var g=t(n,[["render",c]]);export{b as __pageData,g as default};
